cb65b8d7fde2d2196542fedcc90113d0fc949789
livecoderuaridh <ruaridhbell73@googlemail.com>
2026-04-03 16:39:05 +0100

    fix: updated favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import avatar from '$lib/assets/avatar.jpg';
 	import { site } from '$lib/site';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={avatar} />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content={site.name} />
 	<meta property="og:locale" content="en_GB" />
